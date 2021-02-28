8b857d1837154923a96f84da759c255920edc952
Peter Stubbs <peter.stubbs@outlook.com>
2021-02-28 12:39:35 +0000

    update github link

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -2,6 +2,6 @@
 <script>
 document.write('<small><i>URL: ' + document.URL + '</i></small>');
 </script>
-<p>Made with <a href="https://code.google.com/p/petesgallery/">Pete's Gallery</a></p>
+<p>Made with <a href="https://github.com/ps258/gallery">Pete's Gallery</a></p>
 </body>
 </html>
